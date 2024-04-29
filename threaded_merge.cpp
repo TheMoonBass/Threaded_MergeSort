f78b3953285d7e453da726e95e5fc0aeb0e27502
TheMoonBass <pmoon543@gmail.com>
2024-04-29 13:44:55 -0500

    updated for runtime analysis

--- threaded_merge.cpp
+++ threaded_merge.cpp
@@ -1,32 +1,40 @@
 #include "threaded_merge.hpp"
 
 int main() {
-  int max_threads;
-  long int array_size;
+  int max_threads, num_runs;
+  long int array_size, total_time = 0, avg_time;
   std::cout << "Input max threads" << std::endl;
   std::cin >> max_threads;
   std::cout << "Enter array size to sort" << std::endl;
   std::cin >> array_size;
+  std::cout << "Number of test sorts to average" << std::endl;
+  std::cin >> num_runs;
 
-  //init random number generator and array
-  unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
-  srand(seed);
   int* arr = new int[array_size];
+  for(int i=0; i < num_runs; i++ ) {
+    //init random number generator and array
+    unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
+    
 
-  //Fill an array of the given size with random numbers
-  for(int i=0; i<array_size; i++) {
-    arr[i] = rand() % array_size;
+    //Fill an array of the given size with random numbers
+    for(int i=0; i<array_size; i++) {
+      arr[i] = rand() % array_size;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort(arr, array_size, max_threads);
+    auto stop = std::chrono::high_resolution_clock::now();
+    
+    long int runtime = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+    total_time += runtime;
+    
   }
+  avg_time = total_time / num_runs;
 
-  auto start = std::chrono::high_resolution_clock::now();
-  mergeSort(arr, array_size, max_threads);
-  auto stop = std::chrono::high_resolution_clock::now();
-  
-  auto runtime = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  std::cout << "Averge execution time: " << avg_time << " milliseconds" << std::endl; 
 
-  std::cout << "Execution time: " << runtime.count() << " microseconds" << std::endl; 
-
-  //Delete the allocated array and exit
+  //Delete array and exit
   delete[] arr;
   return 0;
 }
